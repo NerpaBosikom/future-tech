af00bcc8eb5de2f27ab55d692259861e9eb2d903
Svetlana <bosekom@bk.ru>
2026-02-16 17:09:02 +0300

    add-upd-js-tabs

--- scripts/Tabs.js
+++ scripts/Tabs.js
@@ -20,11 +20,26 @@ class Tabs {
     this.rootElement = rootElement
     this.buttonElements = this.rootElement.querySelectorAll(this.selectors.button)
     this.contentElements = this.rootElement.querySelectorAll(this.selectors.content)
-    this.state = {
+    this.state = this.getProxyState({
       activeTabIndex: [...this.buttonElements].findIndex((buttonElement) => buttonElement.classList.contains(this.stateClasses.isActive))
-    }
+    })
     this.limitTabsIndex = this.buttonElements.length - 1
     this.bindEvents()
+  }
+
+  getProxyState(initialState) {
+    return new Proxy(initialState, {
+      get: (target, prop) => {
+        return target[prop]
+      },
+      set: (target, prop, value) => {
+        target[prop] = value
+
+        this.updateUI()
+
+        return true
+      },
+    })
   }
 
   updateUI() {
@@ -76,7 +91,6 @@ lastTab = () => {
 
   onButtonClick(buttonIndex) {
     this.state.activeTabIndex = buttonIndex
-    this.updateUI()
   }
 
   onKeyDown = (event) => {
@@ -92,19 +106,16 @@ lastTab = () => {
     const isMacHomeKey = metaKay && code === 'ArrowLeft'
     if (isMacHomeKey) {
       this.firstTab()
-      this.updateUI()
       return
     }
 
     const isMacEndKey = metaKay && code === 'ArrowRight'
     if (isMacHomeKey) {
       this.lastTab()
-      this.updateUI()
       return
     }
 
     action?.()
-    this.updateUI()
   }
 
   bindEvents() {
